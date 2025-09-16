d5108021c28e34b11e758af80465fee26b3ea636
Mariapoda <maria.podanoschi.negri@escola.pr.gov.br>
2025-09-16 12:16:00 -0300

    atualizaçã sons

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'A' 
+// Criação das constatantes do som
+const sons = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'kick.wav',
+    'G' : 'openhat.wav',
+    'H' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+
+}
+//Função para criar uma div para cada tecla
+const criarDiv = (sons) => {
+    div.classList('key');
+    div.textContent = texto;
+}
+exibir(sons);
+document.getElementById('container')
