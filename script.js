d998f7ee2e8f3073fb92854350036e632115762e
Mariapoda <maria.podanoschi.negri@escola.pr.gov.br>
2025-09-25 11:31:38 -0300

    alteração

--- script.js
+++ script.js
@@ -12,9 +12,20 @@ const sons = {
 
 }
 //Função para criar uma div para cada tecla
-const criarDiv = (sons) => {
-    div.classList('key');
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
     div.textContent = texto;
+    div.id =texto;
+    doocument.getElementById('container').appendchild(div);
 }
+
+//criar função para botao da tela
+
+const exibir = Object.keys(sons).forEach(criarDiv)
+
+const adicionarEfeito = (letra) => document.getElementById(letra)
+
+
 exibir(sons);
-document.getElementById('container')
+document.getElementById('container');
